aa68f6bd062cee35da3c1af4f78e15e772660b73
root <root@DickyPrasetyo15.com>
2026-06-15 20:26:35 +0700

    fix: symmetric product cards via Bootstrap flex classes in HTML

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -77,7 +77,7 @@
       <div class="row gy-4 align-items-stretch">
         @forelse ($products as $product)
         <div class="col-6 col-lg-3" data-aos="fade-up" data-aos-delay="200">
-          <article class="card-post h-100">
+          <article class="card-post h-100 d-flex flex-column">
             <div class="post-img position-relative overflow-hidden">
               @if ($product->primaryImage)
                 <img src="{{ $product->primaryImage->url }}" class="img-fluid w-100" alt="{{ $product->name }}" loading="lazy">
@@ -85,14 +85,14 @@
                 <div class="d-flex align-items-center justify-content-center bg-light" style="height:200px"><span class="text-muted">Tanpa Gambar</span></div>
               @endif
             </div>
-            <div class="content">
+            <div class="content d-flex flex-column flex-grow-1">
               <div class="meta d-flex align-items-center flex-wrap gap-2">
                 @foreach ($product->tags as $tag)
                   <span class="cat-badge">{{ $tag->name }}</span>
                 @endforeach
               </div>
-              <h3 class="title">{{ $product->name }}</h3>
-              <h5>{{ $product->priceRangeLabel() }}</h5>
+              <h3 class="title" style="display:-webkit-box;-webkit-line-clamp:2;-webkit-box-orient:vertical;overflow:hidden;min-height:2.7em;">{{ $product->name }}</h3>
+              <h5 class="mb-0">{{ $product->priceRangeLabel() }}</h5>
               <div class="mt-auto pt-3 text-center">
                 <a href="{{ route('products.show', $product) }}" class="btn btn-dark ps-5 pe-5">Detail</a>
               </div>
